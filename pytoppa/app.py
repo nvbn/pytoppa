9eb44cfa5d4f58fd6d88196975db12eedd2fe6ab
nvbn <nvbn.rm@gmail.com>
2013-12-01 16:06:35 +0400

    Update helpers

--- pytoppa/app.py
+++ pytoppa/app.py
@@ -1,29 +1,18 @@
-import sys
 import os
-import setuptools
-import yaml
-
-
-class SetupParser(object):
-    """Parser of setup.py"""
-
-    def _patch_setuptools(self):
-        """Patch setuptools"""
-        setuptools.setup = self._store_args
-
-    def _store_args(self, *args, **kwargs):
-        self._data = kwargs
-
-    def parse(self):
-        """Parse setup.py"""
-        self._patch_setuptools()
-        sys.path.insert(0, os.getcwd())
-        import setup
-        return self._data
+from .contexts.globals import GlobalContext
+from .contexts.release import ReleaseContext
+from .helpers.temporary import TemporaryDirectory
+from .helpers.package_data import PackageData
 
 
 def main():
-    SetupParser().parse()
+    path = os.getcwdu()
+    context = GlobalContext(path)
+    with TemporaryDirectory(path, context) as temp:
+        for release in context['releases']:
+            release_context = ReleaseContext(context, release)
+            with PackageData(temp.destination, release_context):
+                print 'horray'
 
 
 if __name__ == '__main__':

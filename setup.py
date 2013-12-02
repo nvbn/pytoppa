5efbed1afca6d3b53caca3160a5fef275ff6dc89
nvbn <nvbn.rm@gmail.com>
2013-12-02 13:23:30 +0400

    Increase version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytoppa',
-    version='10.0',
+    version='11.0',
     description="Easy to use publisher of python packages to ppa",
     long_description="""\
 """,

25cb7395c513644411d3335cb6c72f7b388c4ec5
nvbn <nvbn.rm@gmail.com>
2014-01-24 06:08:55 +0400

    Fix finding templates

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytoppa',
-    version='13.0',
+    version='14.0',
     description="Easy to use publisher of python packages to ppa",
     long_description="""\
 """,

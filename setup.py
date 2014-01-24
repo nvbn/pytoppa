b4b4f601fed6f840e209bf8dad1d68da1323477b
nvbn <nvbn.rm@gmail.com>
2014-01-24 06:11:01 +0400

    Fix finding source/* templates

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytoppa',
-    version='14.0',
+    version='15.0',
     description="Easy to use publisher of python packages to ppa",
     long_description="""\
 """,
-    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[],
     keywords='',
     author='Vladimir Iakovlev',
     author_email='nvbn.rm@gmail.com',

754710b3b2d27223460f3cd8e8162c4e4612e7c9
nvbn <nvbn.rm@gmail.com>
2014-04-24 17:20:04 +0400

    Add trusty support

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytoppa',
-    version='15.0',
+    version='15.0.1',
     description="Easy to use publisher of python packages to ppa",
     long_description="""\
 """,
